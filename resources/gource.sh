869e4b79f5f93fdf60797dc3ca7a366cb68b5682
Alexander Brandt <brandt.alxndr@outlook.com>
2020-06-08 05:06:00 -0300

    [Resources] Gource

---
+++ resources/gource.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gource --fullscreen --multi-sampling --time-scale 2.0 --elasticity 1.0 --background-colour 000000 -highlight-dirs --caption-file ./resources/gource/captions.txt --user-image-dir ./resources/gource
